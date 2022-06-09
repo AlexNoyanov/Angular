154d0e2294f8fd73d999f9e2be0c7f4333c9d0f9
Alex Noyanov <alex.noyanov@gmail.com>
2022-06-10 00:40:44 +0300

    Basic wallet created, next step is to make the cards

--- Cards/src/app/app.module.ts
+++ Cards/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { WalletComponent } from './wallet/wallet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    WalletComponent
   ],
   imports: [
     BrowserModule
