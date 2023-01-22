681dbe9325d4da9e32df94cefc553f152bfc32e4
Alex Noyanov <alex.noyanov@gmail.com>
2023-01-22 20:26:12 +0300

    Menu component styling

--- MyPortfolioAngularFirst/src/app/menu/menu.component.ts
+++ MyPortfolioAngularFirst/src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
