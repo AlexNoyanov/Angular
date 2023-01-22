3ebcedadb8e3566dee34b0c4a22001f4a5de1bd8
Alex Noyanov <alex.noyanov@gmail.com>
2023-01-22 20:03:49 +0300

    All components created. Building the Menu component

--- MyPortfolioAngularFirst/src/app/app.module.ts
+++ MyPortfolioAngularFirst/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ContactComponent } from './contact/contact.component';
+import {MenuComponent} from "./menu/menu.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
+    ProjectsComponent,
+    TechnologiesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
